ad209e15cd679f638b448b16e11947fc861c086a
k88hudson-cfa <ryl8@cdc.gov>
2025-04-17 10:04:23 -0400

    Fix type issues and apply parameter prop to other types of inputs

--- src/config/parameters.config.ts
+++ src/config/parameters.config.ts
@@ -20,13 +20,16 @@ export const ParameterEditorConfig = defineEditorConfig<Parameters>({
         tooltip: "Mean duration of infectious period, in days",
     },
     fraction_symptomatic: {
-        tooltip: "Proportion of infections that would result in symptoms in the absence of mitigations",
+        tooltip:
+            "Proportion of infections that would result in symptoms in the absence of mitigations",
     },
     fraction_hospitalized: {
-        tooltip: "Proportion of infections that would result in hospitalization in the absence of mitigations",
+        tooltip:
+            "Proportion of infections that would result in hospitalization in the absence of mitigations",
     },
     fraction_dead: {
-        tooltip: "Proportion of infections that would result in death in the absence of mitigations",
+        tooltip:
+            "Proportion of infections that would result in death in the absence of mitigations",
     },
     p_test_sympto: {
         tooltip: "Proportion of symptomatic infections that are tested",
@@ -35,25 +38,31 @@ export const ParameterEditorConfig = defineEditorConfig<Parameters>({
         tooltip: "Proportion of tested infections that are positive",
     },
     p_test_forward: {
-        tooltip: "Proportion of positive tests that are forwarded to public health, resulting in a detection",
+        tooltip:
+            "Proportion of positive tests that are forwarded to public health, resulting in a detection",
     },
     "mitigations.antivirals.ave_i": {
-        tooltip: "Proportional reduction in onward transmission among treated, infected individuals",
+        tooltip:
+            "Proportional reduction in onward transmission among treated, infected individuals",
     },
     "mitigations.antivirals.ave_p": {
-        tooltip: "Proportion of treated, symptomatic infections that avert hospitalization, and of treated, hospitalized individuals who avert death",
+        tooltip:
+            "Proportion of treated, symptomatic infections that avert hospitalization, and of treated, hospitalized individuals who avert death",
     },
     "mitigations.antivirals.fraction_seek_care": {
         tooltip: "Proportion of symptomatic infections that seek care",
     },
     "mitigations.antivirals.fraction_diagnosed_prescribed_outpatient": {
-        tooltip: "Proportion of care-seeking infections that are prescribed outpatient antivirals",
+        tooltip:
+            "Proportion of care-seeking infections that are prescribed outpatient antivirals",
     },
     "mitigations.antivirals.fraction_adhere": {
-        tooltip: "Proportion of individuals prescribed outpatient antivirals who take their course",
+        tooltip:
+            "Proportion of individuals prescribed outpatient antivirals who take their course",
     },
     "mitigations.antivirals.fraction_diagnosed_prescribed_inpatient": {
-        tooltip: "Proportion of hospitalized infections that are prescribed inpatient antivirals",
+        tooltip:
+            "Proportion of hospitalized infections that are prescribed inpatient antivirals",
     },
     "mitigations.community.start": {
         tooltip: "Start day of the mitigation",
@@ -62,10 +71,12 @@ export const ParameterEditorConfig = defineEditorConfig<Parameters>({
         tooltip: "Duration of the mitigation in days",
     },
     "mitigations.community.effectiveness": {
-        tooltip: "Proportional reduction in transmission due to the mitigation, where negative values mean an increase in transmission",
+        tooltip:
+            "Proportional reduction in transmission due to the mitigation, where negative values mean an increase in transmission",
     },
     "mitigations.ttiq.p_id_infectious": {
-        tooltip: "Probability an infectious person will be identified via surveillance",
+        tooltip:
+            "Probability an infectious person will be identified via surveillance",
     },
     "mitigations.ttiq.p_infectious_isolates": {
         tooltip: "Probability an identified, infectious person will isolate",
@@ -77,13 +88,15 @@ export const ParameterEditorConfig = defineEditorConfig<Parameters>({
         tooltip: "Probability contact tracing will identify an exposed person",
     },
     "mitigations.ttiq.p_traced_quarantines": {
-        tooltip: "Probability a traced contact will quarantine and never transmit",
+        tooltip:
+            "Probability a traced contact will quarantine and never transmit",
     },
     "mitigations.vaccine.start": {
         tooltip: "Start day of the vaccination campaign",
     },
     "mitigations.vaccine.doses_available": {
-        tooltip: "Total number of doses available for vaccination. Determines the duration of the vaccine campaign.",
+        tooltip:
+            "Total number of doses available for vaccination. Determines the duration of the vaccine campaign.",
     },
     "mitigations.vaccine.administration_rate": {
         tooltip: "Number of doses administered per day",
@@ -96,5 +109,5 @@ export const ParameterEditorConfig = defineEditorConfig<Parameters>({
     },
     "mitigations.vaccine.ve_p": {
         tooltip: "Vaccine effectiveness against illness",
-    }
+    },
 });
--- src/layout/MiniExpandable.tsx
+++ src/layout/MiniExpandable.tsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 import { Caret } from "./Caret";
 import "./MiniExpandable.css";
+import {
+    ParameterEditorConfig,
+    ParameterPath,
+} from "../config/parameters.config";
 
 type MiniExpandableProps = {
     title: string;
+    parameter?: ParameterPath;
     initialState: boolean;
     children: React.ReactNode;
 };
 
 export function MiniExpandable({
     title,
+    parameter,
     initialState,
     children,
 }: MiniExpandableProps) {
     const [expanded, setExpanded] = useState(initialState);
+    let tooltip =
+        parameter && ParameterEditorConfig.getConfig(parameter)?.tooltip;
     return (
         <div>
             <div
                 className="mini-expandable-header"
+                title={tooltip}
                 onClick={() => setExpanded(!expanded)}
             >
                 <Caret dir={expanded ? "down" : "left"} /> {title}
