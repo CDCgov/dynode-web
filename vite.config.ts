a16dd77dd82694b95e2c1d37d8df8373f38e9fda
k88hudson-cfa <ryl8@cdc.gov>
2025-04-17 18:06:34 -0400

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-    base: process.env.TARGET === "azure" ? "./" : "/dynode-web/",
-    resolve: {
-        alias: {
-            "@wasm": path.resolve(__dirname, "./wasm_dynode/pkg"),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        base: env.TARGET === "azure" ? "./" : "/dynode-web/",
+        resolve: {
+            alias: {
+                "@wasm": path.resolve(__dirname, "./wasm_dynode/pkg"),
+            },
         },
-    },
-    server: { port: 8888 },
-    define: {
-        // Removes in-source testing
-        "import.meta.vitest": "undefined",
-    },
-    plugins: [react(), wasm(), topLevelAwait()],
+        server: { port: 8888 },
+        define: {
+            // Removes in-source testing
+            "import.meta.vitest": "undefined",
+        },
+        plugins: [react(), wasm(), topLevelAwait()],
+    };
 });
