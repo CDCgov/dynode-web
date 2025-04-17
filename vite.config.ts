558cfd30f782849792af822b3bb1ec16fe466f6b
k88hudson-cfa <ryl8@cdc.gov>
2025-04-17 18:14:43 -0400

    Use mode instead of process.env

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
@@ -6,9 +6,8 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), "");
     return {
-        base: env.TARGET === "azure" ? "./" : "/dynode-web/",
+        base: mode === "azure" ? "./" : "/dynode-web/",
         resolve: {
             alias: {
                 "@wasm": path.resolve(__dirname, "./wasm_dynode/pkg"),
